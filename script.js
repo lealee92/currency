b9cdd1799ef8bc6fb9f962555767faf15c9f66dd
Léa LEE <lealee@MacBook-Pro-de-Lea.local>
2022-06-17 16:49:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const currency = document.querySelectorAll(".currency select");
+
+for (let i = 0; i < currency.length; i++) {
+  for (currency_code in country_code) {
+    console.log(currency_code);
+  }
+}
